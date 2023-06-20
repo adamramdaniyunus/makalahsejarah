d8e376b5aded461a04c584671bda27f6ac885a45
adamramdaniyunus <adamramdani1122@gmail.com>
2023-06-20 14:12:04 +0700

    fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { FaInstagram } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <div className='bg-black bottom-0 p-5'>
-            <div className='flex justify-end gap-x-1 items-center'>
+        <div className='bg-black  p-5  w-screen'>
+            <div className='flex justify-end gap-x-1 items-center mr-4'>
                 <a href="https://www.instagram.com/adamramdaniyunus/" className='text-white font-bold text-xl'><FaInstagram /></a>
                 <a href='https://www.instagram.com/adamramdaniyunus/' className='text-white font-bold'>My Instagram</a>
             </div>
